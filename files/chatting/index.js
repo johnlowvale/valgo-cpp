1784ee2954b7b2919da8a659e177c6354b817f96
John Lowvale <johnlowvale@hotmail.com>
2016-12-07 12:15:02 +0700

    added comments in chatbot embed code

--- files/chatting/index.js
+++ files/chatting/index.js
@@ -1,32 +1,52 @@
+/**
+ * vAlgo++ Smart Algorithm Service
+ * Copyright (c) Abivin JSC
+ * @file    Chatbot embed javascript file
+ * @version 0.1
+ * @author  Dat Dinhquoc
+ */
 
+/**
+ * Send text to server
+ */
 function send_text(event) {
+
+  //not enter? just return
   if (event.keyCode!=13)
     return;
 
+  //get text input
   var Textbox = $("#Chat-Text");
   var Text    = Textbox.val().trim();
 
+  //skip empty input
   if (Text.length==0) {
     Textbox.val("");
     return;
   }
 
+  //log the input text
   var Logbox = $("#Chat-Log");
-  var Html   = Logbox.html();
-
-  Html += "Visitor: "+Text+"<br/>";
+  var Html   = Logbox.html()+"Visitor: "+Text+"<br/>";
 
   Logbox.html(Html);
   Logbox.get(0).scrollTop = Number.MAX_VALUE;
   Textbox.val("");
   Textbox.focus();
+
+  //send to server
+  //???
 }
 
+/**
+ * Toggle chatbox 
+ */
 function toggle_chatbox() {
   var Box     = $("#Chat-Box");
   var Button  = $("#Chat-Button");
   var Display = Box.css("display");
 
+  //toggle css display property
   if (Display=="none") {
     Box.css("display","block");
     Button.css("display","none");
